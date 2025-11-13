84242928bc1b9dee27e3667866a6dec612dd0f95
Laith Mimi <laithmimi03@gmail.com>
2025-11-13 10:27:08 +0200

    NEW

--- ex1/bank.py
+++ ex1/bank.py
@@ -1,13 +1,25 @@
-from .utils import BlockHash, PublicKey
+import secrets
+from typing import Dict, List
+
+from .utils import (
+    BlockHash,
+    PublicKey,
+    Signature,
+    TxID,
+    GENESIS_BLOCK_PREV,
+    verify,
+)
 from .transaction import Transaction
 from .block import Block
-from typing import List
 
 
 class Bank:
     def __init__(self) -> None:
         """Creates a bank with an empty blockchain and an empty mempool."""
-        raise NotImplementedError()
+        self._mempool: List[Transaction] = []
+        self._blocks: Dict[BlockHash, Block] = {} #dict is fast O(1) lookup time for blocks
+        self._latest_hash: BlockHash = GENESIS_BLOCK_PREV
+        self._utxo: Dict[TxID, Transaction] = {} #dict is fast O(1) lookup time for unspent transactions
 
     def add_transaction_to_mempool(self, transaction: Transaction) -> bool:
         """
@@ -17,8 +29,28 @@ class Bank:
         (ii) the source doesn't have the coin that he tries to spend
         (iii) there is contradicting tx in the mempool.
         (iv) there is no input (i.e., this is an attempt to create money from nothing)
+        so this mempool basically has the verified txs but not yet committed to the blockchain
         """
-        raise NotImplementedError()
+        if transaction.input is None:
+            return False
+
+        prev_tx = self._utxo.get(transaction.input) 
+        #verify that the coin trying to be spent actually exists in the bank’s unspent set
+        if prev_tx is None:
+            return False
+
+        if any(tx.input == transaction.input for tx in self._mempool): #check if the coin is already being spent in the mempool
+            return False
+
+        if transaction in self._mempool: #check if the tx is already in the mempool
+            return False
+
+        message = self._serialize_for_signature(transaction.input, transaction.output) #serialize the transaction id and the target public key for the signature
+        if not verify(message, transaction.signature, prev_tx.output): #verify the signature of the transaction
+            return False
+
+        self._mempool.append(transaction) #add the transaction to the mempool
+        return True
 
     def end_day(self, limit: int = 10) -> BlockHash:
         """
@@ -27,31 +59,49 @@ class Bank:
         If there are fewer than 'limit' transactions in the mempool, a smaller block is created.
         If there are no transactions, an empty block is created. The hash of the block is returned.
         """
-        raise NotImplementedError()
+        if limit < 0:
+            limit = 0
+
+        to_commit = self._mempool[:limit]
+        self._mempool = self._mempool[len(to_commit):]
+
+        block = Block(to_commit, self._latest_hash)
+        block_hash = block.get_block_hash()
+        self._blocks[block_hash] = block
+        self._latest_hash = block_hash
+
+        for tx in to_commit:
+            if tx.input is not None:
+                self._utxo.pop(tx.input, None)
+            self._utxo[tx.get_txid()] = tx #move on to the next tx in to_commit
+
+        return block_hash
 
     def get_block(self, block_hash: BlockHash) -> Block:
         """
         This function returns a block object given its hash. If the block doesnt exist, an exception is thrown..
         """
-        raise NotImplementedError()
+        if block_hash not in self._blocks: #check if the block exists in the bank's blocks
+            raise KeyError("Block not found") #if not, raise an exception
+        return self._blocks[block_hash] #return the block
 
     def get_latest_hash(self) -> BlockHash:
         """
         This function returns the hash of the last Block that was created by the bank.
         """
-        raise NotImplementedError()
+        return self._latest_hash #return the hash of the last block
 
     def get_mempool(self) -> List[Transaction]:
         """
         This function returns the list of transactions that didn't enter any block yet.
         """
-        raise NotImplementedError()
+        return list(self._mempool) #return the list of transactions in the mempool
 
     def get_utxo(self) -> List[Transaction]:
         """
         This function returns the list of unspent transactions.
         """
-        raise NotImplementedError()
+        return list(self._utxo.values())
 
     def create_money(self, target: PublicKey) -> None:
         """
@@ -59,4 +109,10 @@ class Bank:
         this transaction includes a random string of 48 bytes (so that every two creation transactions are different).
         This function is a secret function that only the bank can use (currently for tests, and will make sense in a later exercise).
         """
-        raise NotImplementedError()
+        random_sig = Signature(secrets.token_bytes(48)) #generate a random signature for the transaction
+        tx = Transaction(output=target, input=None, signature=random_sig) #create a new transaction with the random signature
+        self._mempool.append(tx) #add the transaction to the mempool
+
+    @staticmethod
+    def _serialize_for_signature(txid: TxID, target: PublicKey) -> bytes:
+        return txid + target #serialize the transaction id and the target public key for the signature
--- ex1/wallet.py
+++ ex1/wallet.py
@@ -7,7 +7,12 @@ from typing import Optional
 class Wallet:
     def __init__(self) -> None:
         """This function generates a new wallet with a new private key."""
-        raise NotImplementedError()
+        self._private_key: PrivateKey
+        self._public_key: PublicKey
+        self._private_key, self._public_key = gen_keys() #generate a new private key and a corresponding public key
+        self._utxo: Dict[TxID, Transaction] = {} #dict is fast O(1) lookup time for unspent transactions
+        self._frozen: Set[TxID] = set() #set is fast O(1) lookup time for frozen transactions
+        self._last_seen_block_hash = GENESIS_BLOCK_PREV 
 
     def update(self, bank: Bank) -> None:
         """
@@ -15,7 +20,33 @@ class Wallet:
         Don't read all of the bank's utxo, but rather process the blocks since the last update one at a time.
         For this exercise, there is no need to validate all transactions in the block.
         """
-        raise NotImplementedError()
+        latest_hash = bank.get_latest_hash()
+        if latest_hash == self._last_seen_block_hash:
+            return
+
+        blocks_to_process = []
+        current_hash = latest_hash
+        while current_hash != self._last_seen_block_hash and current_hash != GENESIS_BLOCK_PREV: #check if the current hash is the same as the last seen block hash or the genesis block
+            block = bank.get_block(current_hash) #get the block from the bank
+            blocks_to_process.append(block) #add the block to the list of blocks to process
+            current_hash = block.get_prev_block_hash() #get the previous block hash
+
+        if current_hash != self._last_seen_block_hash: #check if the current hash is not the same as the last seen block hash
+            # current_hash is either GENESIS or a hash unknown to the wallet (should not happen in this exercise)
+            if current_hash != GENESIS_BLOCK_PREV: #check if the current hash is the genesis block
+                raise ValueError("Unknown block chain state encountered during update") #if not, raise an exception
+
+        for block in reversed(blocks_to_process):
+            for tx in block.get_transactions():
+                if tx.input is not None and tx.input in self._utxo:
+                    self._utxo.pop(tx.input, None) #remove the input from the unspent set
+                    self._frozen.discard(tx.input)
+                if tx.output == self._public_key:
+                    self._utxo[tx.get_txid()] = tx #add the transaction to the unspent set
+
+        self._last_seen_block_hash = latest_hash
+        # discard any frozen coins that are no longer tracked
+        self._frozen.intersection_update(self._utxo.keys())
 
     def create_transaction(self, target: PublicKey) -> Optional[Transaction]:
         """
@@ -25,13 +56,22 @@ class Wallet:
         bank just yet (it still wasn't included in a block) then the wallet  should'nt spend it again
         until unfreeze_all() is called. The method returns None if there are no unspent outputs that can be used.
         """
-        raise NotImplementedError()
+        available = [txid for txid in self._utxo if txid not in self._frozen]
+        if not available:
+            return None #if there are no unspent outputs that can be used, return None
+
+        input_txid = available[0]
+        message = self._serialize_for_signature(input_txid, target) #serialize the transaction id and the target public key for the signature
+        signature = sign(message, self._private_key) #sign the message using the private key
+        transaction = Transaction(output=target, input=input_txid, signature=signature) #create a new transaction with the input, output and signature
+        self._frozen.add(input_txid) #add the input to the frozen set
+        return transaction
 
     def unfreeze_all(self) -> None:
         """
         Allows the wallet to try to re-spend outputs that it created transactions for (unless these outputs made it into the blockchain).
         """
-        raise NotImplementedError()
+        self._frozen.clear()
 
     def get_balance(self) -> int:
         """
@@ -40,10 +80,14 @@ class Wallet:
         Coins that the wallet owned and sent away will still be considered as part of the balance until the spending
         transaction is in the blockchain.
         """
-        raise NotImplementedError()
+        return len(self._utxo)
 
     def get_address(self) -> PublicKey:
         """
         This function returns the public address of this wallet (see the utils module for generating keys).
         """
-        raise NotImplementedError()
+        return self._public_key
+
+    @staticmethod
+    def _serialize_for_signature(txid: TxID, target: PublicKey) -> bytes:
+        return txid + target
